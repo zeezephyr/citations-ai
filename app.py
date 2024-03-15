752190f7177ee9441e8c2c440516e90d9081280a
zeezephyr <142696453+zeezephyr@users.noreply.github.com>
2024-03-15 00:14:04 +0000

    add persistent db

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import docarray
+from langchain_community.vectorstores.chroma import Chroma
 
 load_dotenv()
 parser = argparse.ArgumentParser(description="Ask OpenAI a question")
 parser.add_argument('prompt', type=str)
 args = parser.parse_args()
 
+# vectorstore = Chroma.from_documents()
 # vectorstore = docarray.DocArrayHnswSearch.from_documents()
 prompt = ChatPromptTemplate.from_template("{topic}")
 model = ChatOpenAI(model="gpt-4", api_key=os.environ['OPEN_AI_API_KEY'])
--- indexer.py
+++ indexer.py
@@ -1,36 +1,41 @@
 import argparse
 import chromadb
 import itertools
+import json
 import os
 
 from pathlib import Path
 from chromadb.config import Settings
+from xdg_base_dirs import xdg_data_home
 
-#after = #after a given datetime (time since last scan)
+# after = #after a given datetime (time since last scan)
 
-parser = argparse.ArgumentParser(description='Scan an archivebox repository and store embeddings in a vectorstore')
-parser.add_argument('directory', type=str)
-parser.add_argument('-c', '--collection', type=str, default='citations')
+parser = argparse.ArgumentParser(
+    description="Scan an archivebox repository and store embeddings in chromadb"
+)
+parser.add_argument("directory", type=str)
+parser.add_argument("-c", "--collection", type=str, default="citations")
+data_path = os.path.join(xdg_data_home(), "citations-ai", "data")
+parser.add_argument("-p", "--persist-data", type=str, default=data_path)
 # parser.add_argument('-a', '--after', default=after)
 args = parser.parse_args()
 
-client = chromadb.Client(Settings(anonymized_telemetry=False))
+client = chromadb.PersistentClient(path=data_path, settings=Settings(anonymized_telemetry=False))
 collection = client.get_or_create_collection(name=args.collection)
 
-pathlist = Path(args.directory).rglob('htmltotext.txt')
-pathlist = list(itertools.islice(pathlist, 10)) #TODO: Remove. temporarily get the first 10
+pathlist = Path(args.directory).rglob("htmltotext.txt")
+pathlist = list(
+    itertools.islice(pathlist, 10)
+)  # TODO: Remove. temporarily get the first 10
 for file in pathlist:
     file_dir = os.path.dirname(file.resolve())
-    index_file = os.path.join(file_dir, 'index.json')
+    index_file = open(os.path.join(file_dir, "index.json"), "r")
+    index = json.load(index_file)
     with file.open() as f:
-        collection.add(
+        collection.upsert(
+            ids=[index["hash"]],
             documents=[f.read()],
-            ids=[str(f)]
+            metadatas=[{"website": index["base_url"], "domain": index["domain"]}]
         )
 
-results = collection.query(
-    query_texts=['this is a test'],
-    n_results=1
-)
-
-print(results)
+results = collection.query(query_texts=["this is a test"], n_results=1)
